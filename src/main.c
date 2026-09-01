5ec934f5055e3a3579c797001af9bb1ae65655a0
nchakicherla <nchaki6@icloud.com>
2026-08-31 19:55:16 -0700

    add file.c/.h from previous project for easy file reading/writing

--- src/main.c
+++ src/main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdalign.h>
 
 #include "arena.h"
+#include "file.h"
 
 int main(void) {
 	printf("hello, world\n");
 
 	Arena arena;
 	arena_init(&arena);
-
-	char *test_alloc = arena_alloc(&arena, 1000000, alignof(char));
-
-	for (size_t i = 0; i < 1000000; i++) {
-		test_alloc[i] = 'a';
-		printf("%c", test_alloc[i]);
-	}
-	printf("\n");
 
 	arena_term(&arena);
 	return 0;
