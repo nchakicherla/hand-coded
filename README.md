5d69286b4acb4f0935f6151c28e2ceea96c9413d
nchakicherla <nchaki6@icloud.com>
2026-08-31 20:54:13 -0700

    update CMakeLists.txt and README.md; wire up a row and column counter in main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hand-coded
+
+Note: This program uses libcsv.c by Robert Gamble sourced in external/libcsv with its own LGPL license
--- src/main.c
+++ src/main.c
@@ -1,14 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "csv.h"
+
 #include "arena.h"
 #include "file.h"
 
-int main(void) {
-	printf("hello, world\n");
+typedef struct {
+	size_t col;
+	size_t max_col;
+	size_t row;
+} CsvCounter;
 
+static void cb_field(void *field, size_t field_len, void *data) {
+	(void)field;
+	(void)field_len;
+
+	CsvCounter *counter = (CsvCounter *)data;
+
+	printf("%zu ", counter->col);
+	counter->col++;
+	return;
+}
+
+static void cb_row(int c, void *data) {
+	(void)c;
+	CsvCounter *counter = (CsvCounter *)data;
+	printf("<-- row %zu\n", counter->row);
+	counter->row++;
+	if (counter->col > counter->max_col) {
+		counter->max_col = counter->col;
+	}
+	counter->col = 0;
+	return;
+}
+
+int main(void) {
 	Arena arena;
 	arena_init(&arena);
+
+	size_t csv_size;
+	char *csv_buffer = file_read_all(&arena, "./resources/sample.csv", &csv_size);
+
+	CsvCounter counter = { 
+		.col = 0,
+		.max_col = 0,
+		.row = 0
+	};
+
+	struct csv_parser parser;
+	csv_init(&parser, 0);
+	csv_parse(&parser, csv_buffer, csv_size, cb_field, cb_row, &counter);
+	csv_fini(&parser, cb_field, cb_row, &counter);
+	csv_free(&parser);
+
+	printf("total num rows: %zu\n", counter.row);
+	printf("max cols: %zu\n", counter.max_col);
 
 	arena_term(&arena);
 	return 0;
